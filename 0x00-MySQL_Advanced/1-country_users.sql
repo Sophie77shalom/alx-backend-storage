6f17d28c338461f56432419c9ea7c60007870065
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-05 22:55:31 +0300

    tasks

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- Create table
+-- Creates table
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country ENUM ('US', 'CO', 'TN' ) NOT NULL
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL
 );
