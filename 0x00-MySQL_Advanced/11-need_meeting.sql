aa53325848aad6246aa06a44c57b06afd7193c13
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-05 23:14:04 +0300

    tasks

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- creates a view need_meeting  
+
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name 
+FROM students 
+WHERE score < 80 AND 
+(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
