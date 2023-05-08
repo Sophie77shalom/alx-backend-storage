c5d310a76102014e106673809cd0b6f6d7385c9c
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-08 16:07:55 +0300

    databases

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""  changes all topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics 
+    """
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, new_values)
