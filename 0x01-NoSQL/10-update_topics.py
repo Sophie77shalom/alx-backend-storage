19bc578b26221cd1e2fafe0bb48b0f821988613f
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-08 16:10:41 +0300

    databases

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,3 +9,4 @@ def update_topics(mongo_collection, name, topics):
     query = {"name": name}
     new_values = {"$set": {"topics": topics}}
     mongo_collection.update_many(query, new_values)
+    
